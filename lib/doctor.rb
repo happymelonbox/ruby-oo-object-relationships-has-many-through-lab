a0f61335b7445f5432c4a073dd308009b73a30c4
Justin Berger Howes <howes.j.j@gmail.com>
2021-09-02 21:03:50 +1000

    complete

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,32 @@
+class Doctor
 
+    attr_accessor :name, :appointments, :patient
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @appointments = []
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select{|appointment| appointment.doctor == self}
+    end
+
+    def new_appointment(date, patient)
+        new_appt = Appointment.new(date, patient, self)
+        @appointments << new_appt
+        new_appt
+    end
+
+    def patients
+        self.appointments.collect do |appointment| 
+            appointment.patient
+        end
+    end
+end
